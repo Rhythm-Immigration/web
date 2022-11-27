8ca37511f0cdc6b090204bb8bdcc14c04f057d69
karanharika <55457934+karanharika@users.noreply.github.com>
2022-11-27 15:14:39 -0800

    Updates v2.1 - Changed navBar and homeImage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -80,266 +80,74 @@ function Home() {
     <Container>
       < link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"></link>
 
-      <MediaQuery minWidth={1290}>
+      {/* <MediaQuery minWidth={1290}> */}
 
-        <Container className="videoContainer">
-          {/* <video className="videoElement" src={MainClip} autoPlay loop muted /> */}
-          {/* <video className="videoElement" src={MainClip} loop muted /> */}
-          <Image className="videoElement" src={HomeImage} />
+      <Container className="videoContainer">
+        {/* <video className="videoElement" src={MainClip} autoPlay loop muted /> */}
+        {/* <video className="videoElement" src={MainClip} loop muted /> */}
+        <Image className="videoElement" src={HomeImage} />
 
 
-          <Container>
-            <Row className="videoContent">
-              Rhythm Immigration Consultancy Services Inc provides personalized and strategic consultancy services to
-              individuals and families on all Canadian immigration visa categories. We help our clients to accomplish
-              their dream of settling, living, studying and working in Canada by assessing their profiles and finding
-              the best immigration pathways for them. We also provide support to individuals to help them re-unite with
-              their family members in Canada. We are well equipped to successfully handle an array of immigration cases.
-            </Row>
-          </Container>
-        </Container>
-
-        <Container className="whyChoose">
-          <Row align="center" className="gap">
-
-            <Row><h1>WHY CHOOSE US</h1></Row>
+        <Container>
+          <Row className="videoContent">
+            Rhythm Immigration Consultancy Services Inc provides personalized and strategic consultancy services to
+            individuals and families on all Canadian immigration visa categories. We help our clients to accomplish
+            their dream of settling, living, studying and working in Canada by assessing their profiles and finding
+            the best immigration pathways for them. We also provide support to individuals to help them re-unite with
+            their family members in Canada. We are well equipped to successfully handle an array of immigration cases.
           </Row>
         </Container>
+      </Container>
 
-        <Container className="slideBoxDeck gap">
-          <Row className="slideBoxwidth">
-            <Col className="slideBox">Professionally handled by a Regulated Canadian Immigration Consultant
-              <Container className="overlay">
-                <Container className="overlayText">Your file will be handled by a Regulated Immigration Consultant whether is a start to finish file or a review</Container>
-              </Container>
-            </Col>
-            <Col className="slideBox">Free First Assessment
-              <Container className="overlay">
-                <Container className="overlayText">We provide a free of cost initial (first) consultation for a maximum of 15 minutes over the phone (no in-person appointment)</Container>
-              </Container>
-            </Col>
-            <Col className="slideBox">Consultation available over the phone/online.
-              <Container className="overlay">
-                <Container className="overlayText">You do not need to take time off from work, cancel your plans or deal with traffic, we provide consultations over the phone/online for your convenience</Container>
-              </Container>
-            </Col>
-          </Row>
-          <Row className="slideBoxwidth">
-            <Col className="slideBox">Flexible working hours
-              <Container className="overlay">
-                <Container className="overlayText">We have flexible working hours/days and are available on most weekends and statutory holidays with a prior appointment</Container>
-              </Container>
-            </Col>
-            <Col className="slideBox">Fee for Service
-              <Container className="overlay">
-                <Container className="overlayText">We offer our services at market competitive prices with an adjustable payment plan. We charge a flat fee for our services and there are not hidden or hourly charges for visa applications.</Container>
-              </Container>
-            </Col>
-            <Col className="slideBox">Attention to Detail
-              <Container className="overlay">
-                <Container className="overlayText">We assess profile very carefully before giving any immigration advice. Apart from that, we always sit with our clients and check their profile thoroughly before final submission which gives them a sense of confidence and satisfaction</Container>
-              </Container>
-            </Col>
-          </Row>
-        </Container>
+      <Container className="whyChoose">
+        <Row align="center" className="gap">
 
-        <Container className="contactContainer">
-
-          <Row>
-            <Col md={6} className="contactInfoBox">
-              <Row className="contactInfo">
-                <Row className="contactHeading">
-                  <p>Contact Information</p>
-                </Row>
-                <Row>
-                  <p>Mobile: 001 (604) 906-1717</p>
-                </Row>
-                <Row>
-                  <p>Email: info@ryhthmimmigration.consulting</p>
-                </Row>
-                <Row>
-                  <a href="tel:+16049061717"><Button className="directionsButton" variant="outline-primary">Call</Button></a>
-                </Row>
-              </Row>
-
-              <Row className="contactInfo">
-                <Row className="contactHeading"><p>Office Location</p>
-                </Row>
-                <Row><p>10318, Whalley Blwd #1, <br /> Surrey, B.C., V3T 4H4</p>
-                </Row>
-                <Row><a target="_blank" rel="noreferrer" href="https://www.google.com/maps/dir//10318+Whalley+Blvd,+Surrey,+BC+V3T+4H4/@49.1900441,-122.8435607,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x5485d77fe99784f9:0x97e7ad9c4054079c!2m2!1d-122.841372!2d49.1900441!3e0">
-                  <Button className="directionsButton" variant="outline-primary">Get Directions</Button></a>
-                </Row>
-              </Row>
-            </Col>
-
-
-            <Col md={6} className="contactInfoBox">
-
-              <Row className="textLeft landscapeForm">
-
-                <Form noValidate validated={validated} onSubmit={sendEmail}>
-                  <Row className="mb-3">
-                    <Form.Group as={Col} md="12" controlId="fullName">
-                      {/* <Form.Label>Enter your Full name</Form.Label> */}
-                      <p>Enter your Full name</p>
-                      <InputGroup hasValidation>
-                        <Form.Control
-                          required
-                          minLength="3"
-                          type="text"
-                          placeholder="Name"
-                          defaultValue=""
-                          name="fullName"
-                        />
-                        <Form.Control.Feedback></Form.Control.Feedback>
-                        <Form.Control.Feedback type="invalid">
-                          Please enter your name.
-                        </Form.Control.Feedback>
-                      </InputGroup>
-                    </Form.Group>
-                  </Row>
-
-                  <Row className="mb-3">
-
-                    <Form.Group as={Col} md="6" controlId="email">
-                      {/* <Form.Label>Email</Form.Label> */}
-                      <p>Email</p>
-                      <InputGroup hasValidation>
-                        {/* <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text> */}
-                        <Form.Control
-                          required
-                          minLength="4"
-                          type="email"
-                          placeholder="Enter here"
-                          aria-describedby="inputGroupPrepend"
-                          name="email"
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          Please enter a valid email.
-                        </Form.Control.Feedback>
-                      </InputGroup>
-                    </Form.Group>
-
-
-
-                    <Form.Group as={Col} md="6" controlId="phone">
-                      {/* <Form.Label>Mobile</Form.Label> */}
-                      <p>Mobile</p>
-                      <InputGroup hasValidation>
-                        {/* <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text> */}
-                        <Form.Control
-                          required
-                          min="999999999"
-                          type="number"
-                          placeholder="Enter here"
-                          name="phone"
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          Please enter a valid mobile nummber.
-                        </Form.Control.Feedback>
-                      </InputGroup>
-                    </Form.Group>
-                  </Row>
-
-                  <Row>
-                    <Form.Group as={Col} md="12" controlId="message">
-                      {/* <Form.Label>Message</Form.Label> */}
-                      <p>Message</p>
-                      <Form.Control
-                        required
-                        minLength="5"
-                        as="textarea"
-                        rows={4}
-                        placeholder="Enter your querry here...."
-                        name="message" />
-                      <Form.Control.Feedback type="invalid">
-                        Please type your message.
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                  </Row>
-
-                  <Button className="directionsButton" variant="outline-primary" type="submit">Send Message</Button>
-
-                  <Row> {result ? <Result /> : null} </Row>
-                </Form>
-
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-
-        <Row className="gap">
-          <Col className="gap">  </Col>
+          <Row><h1>WHY CHOOSE US</h1></Row>
         </Row>
+      </Container>
 
-      </MediaQuery>
+      <Container className="slideBoxDeck gap">
+        <Row className="slideBoxwidth">
+          <Col className="slideBox">Professionally handled by a Regulated Canadian Immigration Consultant
+            <Container className="overlay">
+              <Container className="overlayText">Your file will be handled by a Regulated Immigration Consultant whether is a start to finish file or a review</Container>
+            </Container>
+          </Col>
+          <Col className="slideBox">Free First Assessment
+            <Container className="overlay">
+              <Container className="overlayText">We provide a free of cost initial (first) consultation for a maximum of 15 minutes over the phone (no in-person appointment)</Container>
+            </Container>
+          </Col>
+          <Col className="slideBox">Consultation available over the phone/online.
+            <Container className="overlay">
+              <Container className="overlayText">You do not need to take time off from work, cancel your plans or deal with traffic, we provide consultations over the phone/online for your convenience</Container>
+            </Container>
+          </Col>
+        </Row>
+        <Row className="slideBoxwidth">
+          <Col className="slideBox">Flexible working hours
+            <Container className="overlay">
+              <Container className="overlayText">We have flexible working hours/days and are available on most weekends and statutory holidays with a prior appointment</Container>
+            </Container>
+          </Col>
+          <Col className="slideBox">Fee for Service
+            <Container className="overlay">
+              <Container className="overlayText">We offer our services at market competitive prices with an adjustable payment plan. We charge a flat fee for our services and there are not hidden or hourly charges for visa applications.</Container>
+            </Container>
+          </Col>
+          <Col className="slideBox">Attention to Detail
+            <Container className="overlay">
+              <Container className="overlayText">We assess profile very carefully before giving any immigration advice. Apart from that, we always sit with our clients and check their profile thoroughly before final submission which gives them a sense of confidence and satisfaction</Container>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
 
-      <MediaQuery maxWidth={1290}>
-        <Container className="videoContainerMobile">
-          <Image className="videoElement" src={HomeImage} />
+      <Container className="contactContainer">
 
-          <Container>
-            <Row className="videoContentMobile">
-              Rhythm Immigration Consultancy Services Inc provides personalized and strategic consultancy services to
-              individuals and families on all Canadian immigration visa categories. We help our clients to accomplish
-              their dream of settling, living, studying and working in Canada by assessing their profiles and finding
-              the best immigration pathways for them. We also provide support to individuals to help them re-unite with
-              their family members in Canada. We are well equipped to successfully handle an array of immigration cases.
-            </Row>
-          </Container>
-        </Container>
-
-        <Container className="whyChoose">
-          <Row align="center" className="gap">
-
-            <Row><h1>WHY CHOOSE US</h1></Row>
-          </Row>
-        </Container>
-
-
-        <Container className="slideBoxDeckMobile gap">
-          <Row className="slideBoxwidth">
-            <Col className="slideBoxMobile">Professionally handled by a Regulated Canadian Immigration Consultant
-              <Container className="overlay">
-                <Container className="overlayText">Your file will be handled by a Regulated Immigration Consultant whether is a start to finish file or a review</Container>
-              </Container>
-            </Col>
-            <Col className="slideBoxMobile">Free First Assessment
-              <Container className="overlay">
-                <Container className="overlayText">We provide a free of cost initial (first) consultation for a maximum of 15 minutes over the phone (no in-person appointment)</Container>
-              </Container>
-            </Col>
-            <Col className="slideBoxMobile">Consultation available over the phone/online.
-              <Container className="overlay">
-                <Container className="overlayText">You do not need to take time off from work, cancel your plans or deal with traffic, we provide consultations over the phone/online for your convenience</Container>
-              </Container>
-            </Col>
-          </Row>
-          <Row className="slideBoxwidth">
-            <Col className="slideBoxMobile">Flexible working hours
-              <Container className="overlay">
-                <Container className="overlayText">We have flexible working hours/days and are available on most weekends and statutory holidays with a prior appointment</Container>
-              </Container>
-            </Col>
-            <Col className="slideBoxMobile">Fee for Service
-              <Container className="overlay">
-                <Container className="overlayText">We offer our services at market competitive prices with an adjustable payment plan. We charge a flat fee for our services and there are not hidden or hourly charges for visa applications.</Container>
-              </Container>
-            </Col>
-            <Col className="slideBoxMobile">Attention to Detail
-              <Container className="overlay">
-                <Container className="overlayText">We assess profile very carefully before giving any immigration advice. Apart from that, we always sit with our clients and check their profile thoroughly before final submission which gives them a sense of confidence and satisfaction</Container>
-              </Container>
-            </Col>
-          </Row>
-        </Container>
-
-        <Container className="contactContainerMobile">
-
-          <Row className="contactInfoBoxMobile">
-            {/* <Col className="contactInfoBox"> */}
-            <Row xs={1} md={1} lg={1} className="contactInfoMobile">
+        <Row>
+          <Col md={6} className="contactInfoBox">
+            <Row className="contactInfo">
               <Row className="contactHeading">
                 <p>Contact Information</p>
               </Row>
@@ -354,7 +162,7 @@ function Home() {
               </Row>
             </Row>
 
-            <Row xs={1} md={1} lg={1} className="contactInfoMobile">
+            <Row className="contactInfo">
               <Row className="contactHeading"><p>Office Location</p>
               </Row>
               <Row><p>10318, Whalley Blwd #1, <br /> Surrey, B.C., V3T 4H4</p>
@@ -363,11 +171,13 @@ function Home() {
                 <Button className="directionsButton" variant="outline-primary">Get Directions</Button></a>
               </Row>
             </Row>
+          </Col>
 
 
-            <Row xs={1} md={1} lg={1} className="contactInfoMobile mobileForm">
-              <Row className="contactHeading"><p>Contact Us</p>
-              </Row>
+          <Col md={6} className="contactInfoBox">
+
+            <Row className="textLeft landscapeForm">
+
               <Form noValidate validated={validated} onSubmit={sendEmail}>
                 <Row className="mb-3">
                   <Form.Group as={Col} md="12" controlId="fullName">
@@ -455,15 +265,16 @@ function Home() {
               </Form>
 
             </Row>
-            {/* </Col> */}
-          </Row>
-        </Container>
-
-        <Row className="gap">
-          <Col className="gap">  </Col>
+          </Col>
         </Row>
+      </Container>
 
-      </MediaQuery >
+      <Row className="gap">
+        <Col className="gap">  </Col>
+      </Row>
+
+      {/* </MediaQuery> */}
+
 
 
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import RCIC from "../../assets/images/RCIClogo.png";
 // import NavDropdown from "react-bootstrap/NavDropdown";
 import "./Navigation.css";
 import { Container, Image } from "react-bootstrap";
@@ -18,12 +20,16 @@ class Navigation extends Component {
                 <div className="inNavBar">
                     <Navbar collapseOnSelect expand="lg" className="" >
                         <Container>
-                            <Navbar.Brand href='/#/'><Image className="logoSize" src={Logo} /></Navbar.Brand>
+                            <Navbar.Brand href='/#/'><Image className="logoSize" src={Logo} /> </Navbar.Brand><Image className="logoSize" src={RCIC} />
                             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                             <Navbar.Collapse id="responsive-navbar-nav">
                                 <Nav className="ml-auto">
                                     <Nav.Link href='/#/'><h4 className="linkColor">HOME</h4></Nav.Link>
                                     <Nav.Link href='/#/services'><h4 className="linkColor">OUR SERVICES</h4></Nav.Link>
+                                    <Nav.Link href=''><h4 className="navInfo">FREE INITIAL CONSULTATION</h4></Nav.Link>
+                                    <Nav.Link href='tel:+16049061717'><Button className="navButton" variant="outline-primary">Call: +1 (604) 906-1717</Button></Nav.Link>
+
+
                                     {/* <Nav.Link href='/#/projects'><h4 className="linkColor">About Us</h4></Nav.Link> */}
                                     {/* <Nav.Link href="#pricing"><h4 className="linkColor">ASSESSMENT</h4></Nav.Link> */}
                                 </Nav>
